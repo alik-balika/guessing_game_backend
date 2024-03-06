260fcdba248d5f89096a2da51155f571ced08c08
alik-balika <balikaalik@gmail.com>
2024-03-05 18:14:05 -0800

    add localhost:5173 to origin list

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/api/players", playersRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://alik-balika.github.io",
+    origin: ["https://alik-balika.github.io", "http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
