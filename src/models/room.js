77633b3fd14531c3926e76cf1203ec96a7493f10
alik-balika <balikaalik@gmail.com>
2024-03-07 01:39:04 -0800

    make rooms expire after 60 seconds, currently testing it first

--- src/models/room.js
+++ src/models/room.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now, expires: 60 },
 });
+
+roomSchema.index({ createdAt: 1 }, { expireAfterSeconds: 0 });
 
 const Room = mongoose.model("Room", roomSchema);
 
